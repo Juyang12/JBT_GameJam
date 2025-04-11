721ab508220cbbf3babdbe9ad8bc7163cbe3a591
CoolHand <2385190668@qq.com>
2025-04-11 12:52:44 +0800

    gm

--- Source/JBT_GameJam/GameMode_f/GM_Level_FlatType.cpp
+++ Source/JBT_GameJam/GameMode_f/GM_Level_FlatType.cpp
@@ -2,4 +2,26 @@
 
 
 #include "GM_Level_FlatType.h"
+#include "GameFramework/DefaultPawn.h" // 默认Pawn
+#include "GameFramework/PlayerController.h" // 默认PlayerController
+#include "GameFramework/HUD.h" // 自定义HUD（可选）
 
+AGM_Level_FlatType::AGM_Level_FlatType()
+{
+    if (CustomPawnClass)
+        DefaultPawnClass = CustomPawnClass;
+    else
+        DefaultPawnClass = ADefaultPawn::StaticClass(); // 默认回退
+
+    if (CustomHUDClass)
+        HUDClass = CustomHUDClass;
+    else
+        HUDClass = AHUD::StaticClass(); // 默认回退
+}
+
+void AGM_Level_FlatType::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
--- Source/JBT_GameJam/GameMode_f/GM_Level_FlatType.h
+++ Source/JBT_GameJam/GameMode_f/GM_Level_FlatType.h
@@ -13,5 +13,20 @@ UCLASS()
 class JBT_GAMEJAM_API AGM_Level_FlatType : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Classes")
+	TSubclassOf<APawn> CustomPawnClass;  // 用于指定蓝图Pawn
+
+	UPROPERTY(EditDefaultsOnly, Category = "Classes")
+	TSubclassOf<AHUD> CustomHUDClass;    // 用于指定蓝图HUD
+
+
+public:
+	AGM_Level_FlatType();
+
+	virtual void BeginPlay() override;
+
+
 	
 };
